24876c013b536454cf92804bb6f14b51e3c39355
Enrico Bellanti <enrico.bellanti@gmail.com>
2020-07-31 16:23:24 +0200

    Correzioni transition - inserimento file js

---
+++ js/script.js
@@ -0,0 +1,3 @@
+document.getElementById('added_playlist_1').innerHTML='Musica relax';
+document.getElementById('added_playlist_2').innerHTML='Buddah Bar';
+document.getElementById('added_playlist_3').innerHTML='Tormentoni';
